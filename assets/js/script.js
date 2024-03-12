fd10c87e4ba1bd7bebc3b03316c7018e7fbeda8f
Daniele206 <danieledeieso2004@gmail.com>
2024-03-12 16:08:58 +0100

    [create] repo, [add] contet to HTML e CSS, [add] img to slider, [add] click to arrow.

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+const {createApp} = Vue;
+
+createApp({
+
+  data(){
+    return{
+      imgContainer: [
+        {
+          img: './assets/img/01.webp',
+          title: 'Spiderman',
+          description: 'Immergiti nei panni di Peter Parker, lunico e inimitabile amichevole spiderman di quartiere.'
+        },
+        {
+          img: './assets/img/02.webp',
+          title: 'Ratchet & Clank',
+          description: 'Ratchet é un meccanico della razza Lombax che grazie al Dimensionatore é pronto a intraprendere mille avventure.'
+        },
+        {
+          img: './assets/img/03.webp',
+          title: 'Fortnite',
+          description: 'Metti play prendi il bus e lanciati sull\'isola, si parte in 100 ma solo uno sopravvive nella Battle-Royale.'
+        },
+        {
+          img: './assets/img/04.webp',
+          title: 'Cat Game',
+          description: 'Impersonifica un gatto nel suo quoitidiano, all\'apparenza potrebbe sembrare noiso ma dietero c\'é molto di piú.'
+        },
+        {
+          img: './assets/img/05.webp',
+          title: 'Marvel Game',
+          description: 'Entra nell\'universo marvel é sconfiggi decine di suoper cattivi utilizzando a turno tutti i tuoi super eroi preferiti.'
+        },
+      ],
+      counter: 0,
+    };
+  },
+
+  methods:{
+    
+  },
+
+  mounted(){
+
+  },
+
+}).mount('#app');
